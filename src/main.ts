c1109614cd34e815bf24c0ec4325747b10b587b1
kim jae won <wjswls456456@naver.com>
2025-05-18 15:24:51 +0900

    chore : 스웨거 적용

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('LiveChat API')
+    .setDescription('유튜브 클론 실식산 채팅 API 명세서')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
   app.useGlobalPipes(new ValidationPipe());
   app.enableShutdownHooks();
   await app.listen(process.env.PORT ?? 3000);
--- src/users/dto/user.dto.ts
+++ src/users/dto/user.dto.ts
@@ -1,16 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({ example: 'user01', description: '로그인 ID' })
   loginId: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({ example: 'securepassword123', description: '비밀번호' })
   loginPassword: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({ example: '재원', description: '닉네임' })
   nickname: string;
 }
 
